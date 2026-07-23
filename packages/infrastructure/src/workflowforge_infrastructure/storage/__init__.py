631560192e8a34c4fe95ea8b8a85b0c0e11357b6
Tatiana Furtuna <68281316+tantyastorm@users.noreply.github.com>
2026-07-23 13:31:11 +0300

    feat(storage): add object-storage S3 foundation

--- packages/infrastructure/src/workflowforge_infrastructure/storage/__init__.py
+++ packages/infrastructure/src/workflowforge_infrastructure/storage/__init__.py
@@ -1,10 +1,19 @@
 """Object storage infrastructure adapters."""
 
 from workflowforge_infrastructure.storage.health import S3HealthCheck
-from workflowforge_infrastructure.storage.s3 import close_s3_client, create_s3_client
+from workflowforge_infrastructure.storage.s3 import (
+    ObjectPromotionError,
+    ObjectStorageError,
+    S3ObjectStorage,
+    close_s3_client,
+    create_s3_client,
+)
 
 __all__ = [
+    "ObjectPromotionError",
+    "ObjectStorageError",
     "S3HealthCheck",
+    "S3ObjectStorage",
     "close_s3_client",
     "create_s3_client",
 ]
